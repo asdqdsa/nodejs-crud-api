d78d935c13cf5944e806404ae62da321987c2872
asdqdsa <asdqdsa@protonmail.com>
2025-11-09 08:25:50 +0100

    refactor: format

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import http, { IncomingMessage, ServerResponse } from 'node:http'
-import { config } from './utils/config.ts'
+import http, { IncomingMessage, ServerResponse } from 'node:http';
+import { config } from './utils/config.ts';
 
-console.log('Booting...')
+console.log('Booting...');
 
-console.log(`PORT: ${config.port}`)
+console.log(`PORT: ${config.port}`);
 
 const server = http.createServer((_: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(
     JSON.stringify({
       data: 'Henlo ',
       msg: 'OK',
     })
-  )
-})
+  );
+});
 
 server.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`)
-})
+  console.log(`Server is running on port ${config.port}`);
+});
